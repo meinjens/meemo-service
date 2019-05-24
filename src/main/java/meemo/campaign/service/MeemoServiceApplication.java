cb73a8fba163973d08814a8a3615def7e501032c
Jens Meinecke <1739138+meinjens@users.noreply.github.com>
2019-05-24 20:42:24 +0200

    ci: ignore Spring Boot Application

--- src/main/java/meemo/campaign/service/MeemoServiceApplication.java
+++ src/main/java/meemo/campaign/service/MeemoServiceApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+import javax.annotation.Generated;
+
 @SpringBootApplication(scanBasePackages = {"meemo.campaign.service"})
 @EnableWebFlux
+@Generated(value="Spring Initializr")
 public class MeemoServiceApplication {
 
 	public static void main(String[] args) {
